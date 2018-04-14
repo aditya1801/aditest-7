2b82067b161fb708b0da0307fc9835ba91a255d0
aditya1801 <lit2016023@iiita.ac.in>
2018-04-14 13:27:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ socket.on('chat message', function(msg){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
